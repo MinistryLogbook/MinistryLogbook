f363106d25b5e6951d184c24d849fdc9cdd3a8f7
Daniel Däschle <daniel.daeschle@gmail.com>
2023-04-07 00:04:52 +0200

    feat: use material date picker

--- app/src/main/java/com/github/danieldaeschle/ministrynotes/ui/home/recorddetails/EntryDetailsBottomSheetContent.kt
+++ app/src/main/java/com/github/danieldaeschle/ministrynotes/ui/home/recorddetails/EntryDetailsBottomSheetContent.kt
@@ -9,21 +9,25 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -35,14 +39,18 @@ import com.github.danieldaeschle.ministrynotes.data.rememberSettingsDataStore
 import com.github.danieldaeschle.ministrynotes.ui.LocalAppNavController
 import com.github.danieldaeschle.ministrynotes.ui.home.HomeGraph
 import com.github.danieldaeschle.ministrynotes.ui.home.viewmodels.EntryDetailsViewModel
-import com.marosseleng.compose.material3.datetimepickers.date.ui.dialog.DatePickerDialog
+import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atTime
+import kotlinx.datetime.toInstant
 import kotlinx.datetime.toJavaLocalDate
+import kotlinx.datetime.toLocalDateTime
 import org.koin.androidx.compose.koinViewModel
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EntryDetailsBottomSheetContent(
     id: Int? = null,
@@ -51,19 +59,36 @@ fun EntryDetailsBottomSheetContent(
     val navController = LocalAppNavController.current
     val backStackEntry = navController.currentBackStackEntryAsState()
     val currentRoute = backStackEntry.value?.destination?.route
-    val entry = entryDetailsViewModel.entry.collectAsState()
-    val isSavable = entry.value.let {
-        it.hours > 0 || it.minutes > 0 || it.returnVisits > 0 || it.placements > 0
-                || it.videoShowings > 0
+    val entry by entryDetailsViewModel.entry.collectAsState()
+    val isSavable by remember {
+        derivedStateOf {
+            entry.let {
+                it.hours > 0 || it.minutes > 0 || it.returnVisits > 0 || it.placements > 0
+                        || it.videoShowings > 0
+            }
+        }
     }
-    val hasLoaded = (id ?: 0) == entry.value.id
+    val hasLoaded = (id ?: 0) == entry.id
     var isDateDialogVisible by rememberSaveable { mutableStateOf(false) }
     var isDeleteDialogVisible by rememberSaveable { mutableStateOf(false) }
     var isEntryKindDialogVisible by rememberSaveable { mutableStateOf(false) }
     val settingsDataStore = rememberSettingsDataStore()
-    val role = settingsDataStore.role.collectAsState(Role.Publisher)
+    val role by settingsDataStore.role.collectAsState(Role.Publisher)
     val dateTimeFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)
-    val isCreditEnabled = role.value.canHaveCredit || entry.value.isCredit
+    val isCreditEnabled = role.canHaveCredit || entry.isCredit
+    val dateMillis by remember {
+        derivedStateOf {
+            entry.datetime.atTime(0, 0).toInstant(TimeZone.currentSystemDefault())
+                .toEpochMilliseconds()
+        }
+    }
+    val datePickerState = rememberDatePickerState(
+        initialSelectedDateMillis = dateMillis,
+        initialDisplayedMonthMillis = dateMillis,
+    )
+    val datePickerConfirmEnabled by remember {
+        derivedStateOf { datePickerState.selectedDateMillis != null }
+    }
 
     val handleClose: () -> Unit = {
         isDateDialogVisible = false
@@ -83,13 +108,13 @@ fun EntryDetailsBottomSheetContent(
         }
     }
     val handleChangeMinutes: (newValue: Int) -> Unit = {
-        if (entry.value.hours > 0 && it < 0) {
+        if (entry.hours > 0 && it < 0) {
             entryDetailsViewModel.update(
-                hours = entry.value.hours - 1, minutes = 60 + it
+                hours = entry.hours - 1, minutes = 60 + it
             )
         } else if (it > 59) {
             entryDetailsViewModel.update(
-                hours = entry.value.hours + 1, minutes = it - 60
+                hours = entry.hours + 1, minutes = it - 60
             )
         } else if (it in 0..55) {
             entryDetailsViewModel.update(minutes = it)
@@ -128,17 +153,37 @@ fun EntryDetailsBottomSheetContent(
         val onDismissRequest = {
             isDateDialogVisible = false
         }
-        val onDateChange: (newDate: java.time.LocalDate) -> Unit = {
-            val date = LocalDate(it.year, it.monthValue, it.dayOfMonth)
-            handleChangeDate(date)
+        val onDateChange: () -> Unit = {
+            datePickerState.selectedDateMillis?.let {
+                val dateTime = Instant.fromEpochMilliseconds(it).toLocalDateTime(
+                    TimeZone.currentSystemDefault()
+                )
+                handleChangeDate(dateTime.date)
+            }
             isDateDialogVisible = false
         }
         DatePickerDialog(
             onDismissRequest = onDismissRequest,
-            onDateChange = onDateChange,
-            initialDate = entry.value.datetime.toJavaLocalDate(),
-            shape = RoundedCornerShape(16.dp),
-        )
+            confirmButton = {
+                TextButton(
+                    onClick = onDateChange,
+                    enabled = datePickerConfirmEnabled
+                ) {
+                    Text("OK")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        isDateDialogVisible = false
+                    }
+                ) {
+                    Text("Cancel")
+                }
+            }
+        ) {
+            DatePicker(state = datePickerState)
+        }
     }
 
     if (isDeleteDialogVisible) {
@@ -179,9 +224,9 @@ fun EntryDetailsBottomSheetContent(
         paddingValues = PaddingValues(vertical = 8.dp),
     ) {
         val entryKinds =
-            if (role.value.canHaveCredit) EntryKind.values() else arrayOf(
+            if (role.canHaveCredit) EntryKind.values() else arrayOf(
                 EntryKind.Ministry,
-                entry.value.kind
+                entry.kind
             )
 
         Column {
@@ -198,7 +243,7 @@ fun EntryDetailsBottomSheetContent(
                 ) {
                     RadioButton(
                         modifier = Modifier.padding(horizontal = 20.dp),
-                        selected = it == entry.value.kind,
+                        selected = it == entry.kind,
                         onClick = null,
                     )
                     Text(it.translate())
@@ -214,13 +259,13 @@ fun EntryDetailsBottomSheetContent(
             onSave = handleSave,
             isSavable = isSavable,
             onDelete = { isDeleteDialogVisible = true },
-            isDeletable = entry.value.id != 0,
+            isDeletable = entry.id != 0,
         )
         Divider()
         Box(Modifier.clickable { isDateDialogVisible = true }) {
             Box(Modifier.padding(bottom = 12.dp, start = 20.dp, top = 12.dp, end = 20.dp)) {
                 val dateStr = dateTimeFormatter.format(
-                    entry.value.datetime.toJavaLocalDate()
+                    entry.datetime.toJavaLocalDate()
                 )
                 UnitRow(dateStr, icon = painterResource(R.drawable.ic_today))
             }
@@ -230,8 +275,8 @@ fun EntryDetailsBottomSheetContent(
             Box(Modifier.clickable { isEntryKindDialogVisible = true }) {
                 Box(Modifier.padding(bottom = 12.dp, start = 20.dp, top = 12.dp, end = 20.dp)) {
                     UnitRow(
-                        entry.value.kind.translate(),
-                        icon = entry.value.kind.icon(),
+                        entry.kind.translate(),
+                        icon = entry.kind.icon(),
                     )
                 }
             }
@@ -243,36 +288,36 @@ fun EntryDetailsBottomSheetContent(
                 .fillMaxWidth()
         ) {
             UnitRow("Hours", icon = painterResource(R.drawable.ic_schedule)) {
-                NumberPicker(entry.value.hours) {
+                NumberPicker(entry.hours) {
                     handleChangeHours(it)
                 }
 
             }
             UnitRow("Minutes") {
-                NumberPicker(entry.value.minutes, step = 5) {
+                NumberPicker(entry.minutes, step = 5) {
                     handleChangeMinutes(it)
                 }
             }
 
             if (hasLoaded) {
-                AnimatedVisibility(visible = entry.value.kind == EntryKind.Ministry) {
+                AnimatedVisibility(visible = entry.kind == EntryKind.Ministry) {
                     Column {
                         UnitRow(
                             "Placements",
                             description = "Printed and Electronic",
                             icon = painterResource(R.drawable.ic_article)
                         ) {
-                            NumberPicker(entry.value.placements) {
+                            NumberPicker(entry.placements) {
                                 handleChangePlacements(it)
                             }
                         }
                         UnitRow("Videos", icon = painterResource(R.drawable.ic_play_circle)) {
-                            NumberPicker(entry.value.videoShowings) {
+                            NumberPicker(entry.videoShowings) {
                                 handleChangeVideos(it)
                             }
                         }
                         UnitRow("Return visits", icon = painterResource(R.drawable.ic_group)) {
-                            NumberPicker(entry.value.returnVisits) {
+                            NumberPicker(entry.returnVisits) {
                                 handleChangeReturnVisits(it)
                             }
                         }
