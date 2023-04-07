b87f34fe73ee028d6a53c919e9c8a9a57166ab03
Daniel Däschle <daniel.daeschle@gmail.com>
2023-04-07 23:49:05 +0200

    feat: automatically focus text field

--- app/src/main/java/com/github/danieldaeschle/ministrynotes/ui/settings/NamePage.kt
+++ app/src/main/java/com/github/danieldaeschle/ministrynotes/ui/settings/NamePage.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,8 +16,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.github.danieldaeschle.ministrynotes.R
 import com.github.danieldaeschle.ministrynotes.data.rememberSettingsDataStore
@@ -30,12 +35,19 @@ fun NamePage() {
     val navController = LocalAppNavController.current
     val name by settingsDataStore.name.collectAsState("")
     val coroutineScope = rememberCoroutineScope()
-    var nameValue by remember(name) { mutableStateOf(name) }
+    var textFieldValueState by remember(name) {
+        mutableStateOf(TextFieldValue(text = name, selection = TextRange(name.length)))
+    }
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
 
     BaseSettingsPage("Name", actions = {
         ToolbarAction(onClick = {
             coroutineScope.launch {
-                settingsDataStore.setName(nameValue)
+                settingsDataStore.setName(textFieldValueState.text)
             }
             navController.popBackStack()
         }) {
@@ -44,14 +56,17 @@ fun NamePage() {
     }) {
         Box(Modifier.padding(16.dp)) {
             TextField(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .focusRequester(focusRequester),
                 keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Words),
                 singleLine = true,
                 label = {
                     Text("Set name")
                 },
-                value = nameValue,
-                onValueChange = { nameValue = it })
+                value = textFieldValueState,
+                onValueChange = { textFieldValueState = it },
+            )
         }
     }
 }
