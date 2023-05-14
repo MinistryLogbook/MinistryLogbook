21ae7265297010ceeb6c6792f9f9118c69ceae9f
Daniel Däschle <daniel.daeschle@gmail.com>
2023-05-15 00:05:40 +0200

    fix: path of jks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file("../keystore/MinistryLogbook.jks")
+            storeFile = project.rootDir.resolve("keystore/MinistryLogbook.jks") // file("../keystore/MinistryLogbook.jks")
             storePassword = System.getenv("SIGNING_STORE_PASSWORD")
             keyAlias = System.getenv("SIGNING_KEY_ALIAS")
             keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
